39c50795e04fecf97a6636b4e3ecc962066f6e35
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-09-19 07:54:45 -0300

    Tipos de Saída em JS

---
+++ Aula002 - Tipos de Saída/js/index.js
@@ -0,0 +1,8 @@
+// innerHTML insere texto em alguma tag através do id
+document.getElementById("texto").innerHTML = "Meu primeiro texo <b>JS</b>!";
+
+// cria uma janela de alerta na tela
+alert("Oi, isso é um alerta!");
+
+// faz uma saída na área console de desenvolvimento de uma página, oculta para o usuário
+console.log("Oi, isso é um console.log()!");
