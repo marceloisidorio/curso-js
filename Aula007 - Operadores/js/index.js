420af3b6fe2d563d51be048aa48f836b65210a15
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-09-20 10:08:30 -0300

    Operadores em JS

---
+++ Aula007 - Operadores/js/index.js
@@ -0,0 +1,16 @@
+/*
+
+1) Operadores Aritméticos [+, -, *, /]
+2) Operadores de Atribuição [=, +=, *=, -=, ..]
+3) Operadores de Sequência [total = "string" + "string"]
+4) Operadores de Comparação [==, ===, >, <, ..]
+5) Operadores Condicionais (Ternário) [?]
+6) Operadores Lógicos [&&, ||,  !]
+
+*/
+
+var idade, eleitor;
+idade = 18;
+eleitor = (idade < 18) ? "Não eleitor":"Sim eleitor";
+
+alert(eleitor)
